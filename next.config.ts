03e496a9a6258653908fbcf2c756f0f37b947def
dviela12827 <adalmirpsantos@gmail.com>
2025-10-20 19:57:15 -0300

    Create next.config.ts

---
+++ next.config.ts
@@ -0,0 +1,21 @@
+import type {NextConfig} from 'next';
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'i.imgur.com',
+      },
+    ],
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+};
+
+export default nextConfig;
